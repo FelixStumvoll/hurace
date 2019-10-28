780d83dbc3b503f78f0378fc56d13ec022697f5a
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-10-28 17:49:20 +0100

    creates querybuilder

---
+++ src/Hurace/Hurace.Core/Hurace.Core.Common/QueryBuilder/QueryBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Hurace.Core.Common.QueryBuilder
+{
+    public class QueryBuilder
+    {
+        public class SelectQueryBuilder : QueryBuilder
+        {
+            
+        }
+
+        public class UpdateQueryBuilder : QueryBuilder
+        {
+            
+        }
+
+        public class InsertQueryBuilder : QueryBuilder
+        {
+            
+        }
+    }
+}
