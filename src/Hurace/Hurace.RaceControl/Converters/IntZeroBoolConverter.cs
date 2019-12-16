9e32069098f57a080c605d200ff938aca9d91ab5
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-12-16 13:46:02 +0100

    implements race ranking view

---
+++ src/Hurace/Hurace.RaceControl/Converters/IntZeroBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Hurace.RaceControl.Converters
+{
+    public class IntZeroBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            return ((int) value) == 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
